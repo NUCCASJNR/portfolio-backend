f5b78c500bcb6454a58ad7f126b73568f968de9e
NUCCASJNR <adegbitealareef28@yahoo.com>
2024-01-21 00:02:09 +0100

    update allowed hosts

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wy(zi2rj7byib@ohbd2uq06w=t1r3l6313qf(zot+f=wpaj&w6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
